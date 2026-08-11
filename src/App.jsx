7018aeb5444f11f513c73d44f85171fa47da60f9
Bhumil Khunt <khuntbhumil06@gmail.com>
2026-08-11 15:25:40 +0530

    Chips.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './pages/css/seller.css';
 import './pages/css/snack.css';
 import './pages/css/chip.css'; 
 import './pages/css/premiumnuts.css'; 
+import './pages/css/seed.css'; 
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Homepage';
 import CartPage from './pages/CartPage';
@@ -19,9 +20,11 @@ import { CartProvider } from './context/CartContext';
 import SignupPage from './pages/SignupPage';
 import Seller from './pages/Seller';
 import CheckoutPage from './pages/CheckoutPage';
+import Seeds from './pages/Seeds';
 import Snacks from './pages/Snacks';
 import Chips from './pages/Chips';
 import PremiumNuts from './pages/PremiumNuts';
+
 
 function App() {
   return (
@@ -41,6 +44,7 @@ function App() {
             <Route path='/promotions' element={<Promotional />} />
             <Route path='/about' element={<AboutPage />} />
             <Route path='/arrivals' element={<Arrivals />} />
+            <Route path='/seeds' element={<Seeds />} />
             <Route path='/nuts' element={<PremiumNuts />} />
             <Route path='/snacks' element={<Snacks />} />
             <Route path='/checkout' element={<CheckoutPage />} />
